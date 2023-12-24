e46455d8ad448323d376506a0f013ce5420b1dbd
aditya <nadityanayak0608@gmail.com>
2023-12-24 19:06:21 +0530

    Parallel Execution

--- src/test/java/tests/Test3.java
+++ src/test/java/tests/Test3.java
@@ -9,22 +9,21 @@ import org.testng.annotations.Test;
 
 import resources.Baseclass;
 
-import org.testng.annotations.Test;
-
-public class Test2 extends Baseclass {
+public class Test3 extends Baseclass {
 	WebDriver driver;
 	Logger log;
 
 	@Test
 	public void test2() throws IOException, InterruptedException {
-		System.out.println("Test 2");
+		System.out.println("Test 3");
 		log = LogManager.getLogger(LoginTest.class.getName());
 
 		driver = initializebrowser();
 		log.debug("Browser got launched");
-		driver.get(prop.getProperty("url2"));
+		driver.get(prop.getProperty("url3"));
 		log.debug("Navigated to application URL");
 		Thread.sleep(2000);
 		driver.close();
 	}
+
 }
